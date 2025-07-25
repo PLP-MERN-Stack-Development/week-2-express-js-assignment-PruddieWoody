6d57fc16f320ddc67c573c2d1118c1465bf9be84
PruddieWoody <pru.gladysw@gmail.com>
2025-07-25 14:53:56 +0300

    validate.js

---
+++ validate.js
@@ -0,0 +1,17 @@
+const ValidationError = require('../utils/ValidationError');
+
+function validateProduct(req, res, next) {
+  const { name, description, price, category, inStock } = req.body;
+  if (
+    typeof name !== 'string' ||
+    typeof description !== 'string' ||
+    typeof price !== 'number' ||
+    typeof category !== 'string' ||
+    typeof inStock !== 'boolean'
+  ) {
+    return next(new ValidationError('Invalid product data'));
+  }
+  next();
+}
+
+module.exports = { validateProduct };
